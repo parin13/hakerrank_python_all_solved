f570986af56d42553bee98390842a2cba26f22c8
parin13 <parishilan@auxesisgroup.com>
2019-05-28 16:09:08 +0530

    header

--- python_If-Else.py
+++ python_If-Else.py
@@ -1,25 +1,14 @@
-'''
+#!/bin/python3
+__author__ = 'parishilan'
 
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
-
-'''
-
-
-#!/bin/python
-
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-# n = int(raw_input())
-n = 4
-
+N = int(input())
+n = N
 if not (n%2 == 0):
-	print ("Weird")
-else:	
-	if n in  list(range(2,5)):
-		print ("Not Weird")
-	elif n in list(range(6,21)):
-		print("Weird")
-	else:
-		print ("Not Weird")
+    print ("Weird")
+else:    
+    if n in  list(range(2,5)):
+        print ("Not Weird")
+    elif n in list(range(6,21)):
+        print("Weird")
+    else:
+        print ("Not Weird")
